35d60a0fffc8fdaba3d38293f8307c9137cf2588
Jesus <albertchalare@gmail.com>
2024-10-01 00:24:47 -0500

    payments ms

---
+++ src/config/envs.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  STRIPE_SECRET_KEY: string;
+
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
+}
+const envSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    STRIPE_SECRET_KEY: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  stripe_secret_key: envVars.STRIPE_SECRET_KEY,
+  stripe_success_url: envVars.STRIPE_SUCCESS_URL,
+  stripe_cancel_url: envVars.STRIPE_CANCEL_URL,
+};
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripe_secret_key);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100), // Convert to cents
+      },
+      quantity: item.quantity,
+    }));
+    const session = await this.stripe.checkout.sessions.create({
+      //Id de orden
+      payment_intent_data: {
+        metadata: { orderId: orderId },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: envs.stripe_success_url,
+      cancel_url: envs.stripe_cancel_url,
+    });
+
+    return session;
+  }
+}
